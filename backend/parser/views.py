27603cd59e76a1c90f36dcc7d7cb79f9d815f651
Himanshu <gghimanshu333@gmail.com>
2024-07-26 16:17:16 +0530

    configuring database

--- backend/parser/views.py
+++ backend/parser/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import os
+from tabula import read_pdf, convert_into
+import pandas as pd
 
 # Create your views here.
 def home(request):
@@ -13,10 +17,28 @@ def home(request):
 
 def fileInput(request):
     try:
+        
         if request.method == "POST":
+            
             #trying to access files but failing so badly
             #god help me
             return JsonResponse({"message":"successfully"})
         
     except (KeyError, ValueError):
         return JsonResponse({"message":"invalid method"})
+    
+def test():
+    pdf_path = os.path.join(settings.BASE_DIR, 'parser', 'static', 'parser','test.pdf')
+    if os.path.exists(pdf_path):
+        dfs = read_pdf(pdf_path,pages='all')
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        for i in dfs:
+            lista = i.values.tolist()
+            print(lista)
+        print('********************************************************')
+        
+        
+    else:
+        print("no sir")
+
+test()
